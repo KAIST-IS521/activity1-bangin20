ea241733210da932000d0d75461acbe7a711e2ed
Changhun Song <idodiiid@gmail.com>
2019-03-03 21:23:27 +0900

    test added, csv validation added

--- main.py
+++ main.py
@@ -1,14 +1,29 @@
 #!/usr/bin/python3
 import csv, sys
+import mimetypes
 
 if len(sys.argv) != 3:
-	sys.exit(1)	# parameter required
+    sys.exit(1)	# parameter required
+
+dialect = 0
+header = False
 
 with open(sys.argv[1], 'r') as csv_file:
-	N = int(sys.argv[2])
-	csv_reader = csv.reader(csv_file)
+    N = int(sys.argv[2])
+    sniffer = csv.Sniffer()
 
-	for idx, row in enumerate(csv_reader):
-		if idx == N-1:
-			print(row)
-	
+    s = csv_file.read()
+    try:
+        # Validate CSV File
+        dialect = sniffer.sniff(s)
+    except:
+        print("Invalid CSV File Format")
+        sys.exit(1)
+    print(sniffer.has_header(s))
+
+with open(sys.argv[1], 'r') as csv_file:
+    csv_reader = csv.reader(csv_file, dialect=dialect)
+
+    for idx, row in enumerate(csv_reader):
+        if idx == N-1:
+            print(row)
