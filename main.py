eb73d905bbea8f1af84eeaab03ddc59f0db0579c
Changhun Song <idodiiid@gmail.com>
2019-03-06 11:07:42 +0900

    Exception more handled

--- main.py
+++ main.py
@@ -3,27 +3,38 @@ import csv, sys
 import mimetypes
 
 if len(sys.argv) != 3:
+    print("USAGE ./main.py [FILE_NAME] [LINE_NUM]")
     sys.exit(1)	# parameter required
 
 dialect = 0
 header = False
+try:
+    with open(sys.argv[1], 'r') as csv_file:
+        N = int(sys.argv[2])
+        sniffer = csv.Sniffer()
 
-with open(sys.argv[1], 'r') as csv_file:
-    N = int(sys.argv[2])
-    sniffer = csv.Sniffer()
+        s = csv_file.read()
+        try:
+            # Validate CSV File
+            dialect = sniffer.sniff(s)
+        except:
+            print("Invalid CSV File Format")
+            sys.exit(1)
 
-    s = csv_file.read()
-    try:
-        # Validate CSV File
-        dialect = sniffer.sniff(s)
-    except:
-        print("Invalid CSV File Format")
-        sys.exit(1)
-    print(sniffer.has_header(s))
+        if dialect.delimiter != ',':
+            print('Invalid Delimiter')
+            sys.exit(1)
 
-with open(sys.argv[1], 'r') as csv_file:
-    csv_reader = csv.reader(csv_file, dialect=dialect)
+        header = sniffer.has_header(s)
+        lines = s.split('\n')
+        max_column = len(lines[0].split(','))
 
-    for idx, row in enumerate(csv_reader):
-        if idx == N-1:
-            print(row)
+        if N == 0 or N > max_column:
+            print("Invalid Column Number")
+            sys.exit(1)
+
+        target_column = ('\n').join(list(i.split(',')[N-1] for i in lines))
+        print(target_column)
+except:
+    print('File Does Not Exist')
+    sys.exit(1)
