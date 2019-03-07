445d767444d5ac5f4ebe026aba76abb984960c0a
Changhun Song <idodiiid@gmail.com>
2019-03-07 15:02:39 +0900

    add README, bug fixed

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-# FIXME
+# Simple CSV Column Printer
 
-You should modify this file to give concise description about your project.
+---
+
+
+
+**<div style="text-align: right"> GSIS 20193313 Changhun Song </div>**
+
+
+
+### Description
+
+This is for IS521 *activity1* to train how to use git and github.
+
+This program developed on python 3.7, but it still works in the other python3 versions
+
+- How to Use
+  - python3 main.py [CSV_FILE] [COLUMN_NUM]
+  - COLUMN_NUM starts from 1, Not 0. (0 Column number will raise an error)
+  - Any errors will return exit code 1
+
+
+
+
+
+### Reference
+
+CSV Format [RFC4180](https://tools.ietf.org/html/rfc4180).
+
+Test Data Set - From my Undergraduate Course "Big Data" Assignment Data
+
+Python CSV Module https://docs.python.org/3.7/library/csv.html#csv-contents
--- main.py
+++ main.py
@@ -1,40 +1,56 @@
 #!/usr/bin/python3
 import csv, sys
-import mimetypes
 
-if len(sys.argv) != 3:
-    print("USAGE ./main.py [FILE_NAME] [LINE_NUM]")
-    sys.exit(1)	# parameter required
+def ValidateCSV(file_content):
+    sniffer = csv.Sniffer()
+    try:
+        # Validate CSV File with comma separator
+        dialect = sniffer.sniff(file_content, delimiters=',')
+        return dialect
+    except:
+        raise csv.Error
 
-dialect = 0
-header = False
-try:
-    with open(sys.argv[1], 'r') as csv_file:
+def GetNthColumn(csv_file, N, dialect):
+    csv_rows = csv.reader(csv_file, dialect=dialect)
+
+    target_column = []
+    for i, row in enumerate(csv_rows):
+        if i == 0:
+            max_column = len(row)
+            if N == 0 or N > max_column:
+                raise ValueError
+        target_column.append(row[N-1])
+
+    result = '\n'.join(target_column) # parse list with enter separator
+    return result
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("USAGE ./main.py [FILE_NAME] [LINE_NUM]")
+        sys.exit(1)	# parameter required
+
+    file_name = sys.argv[1]
+
+    try:
         N = int(sys.argv[2])
-        sniffer = csv.Sniffer()
+        with open(file_name, 'r') as csv_file:
+            file_content = csv_file.read()
+            try:
+                dialect = ValidateCSV(file_content)
+                csv_file.seek(0)
+                result = GetNthColumn(csv_file, N, dialect)
 
-        s = csv_file.read()
-        try:
-            # Validate CSV File
-            dialect = sniffer.sniff(s)
-        except:
-            print("Invalid CSV File Format")
-            sys.exit(1)
-
-        if dialect.delimiter != ',':
-            print('Invalid Delimiter')
-            sys.exit(1)
-
-        header = sniffer.has_header(s)
-        lines = s.split('\n')
-        max_column = len(lines[0].split(','))
-
-        if N == 0 or N > max_column:
-            print("Invalid Column Number")
-            sys.exit(1)
-
-        target_column = ('\n').join(list(i.split(',')[N-1] for i in lines))
-        print(target_column)
-except:
-    print('File Does Not Exist')
-    sys.exit(1)
+                print(result)
+            except csv.Error:
+                print("Invalid CSV Format")
+                sys.exit(1)
+            except ValueError:
+                print('Invalid N range')
+                sys.exit(1)
+    except FileNotFoundError:
+        print('File Does Not Exist')
+        sys.exit(1)
+    except ValueError:
+        print("Column number should be a Integer")
+        sys.exit(1)
